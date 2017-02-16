13ea4ecf1992df81159290ab60a336f050ca8bca
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-02-16 23:44:25 +0530

    watch container class

--- src/containers/Root/App/Watch/index.js
+++ src/containers/Root/App/Watch/index.js
@@ -1,3 +1,61 @@
 /**
  * Created by jyothi on 16/2/17.
  */
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import {  } from './actions';
+
+function mapStateToProps(state) {
+    return {
+        session: state.session,
+        router: state.routing,
+        overview: state.overview
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+
+    };
+}
+
+class Overview extends Component {
+
+    componentWillMount(){
+
+    }
+
+    componentWillUnmount(){
+        this.props.resetData();
+    }
+
+    render() {
+        const { appState, params, location } = this.props;
+        const { v } = location.query;
+        return (
+            <section className="content">
+                <div className="row">
+                    <div className="col-md-8">
+
+                    </div>
+                    <div className="col-md-4">
+
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
+
+Overview.propTypes = {
+
+};
+
+Overview.contextTypes = {
+    router: PropTypes.object.isRequired,
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Overview);

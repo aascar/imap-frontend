975ee20577fb046b7719a8ca35953242ad25d388
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-02-16 23:43:38 +0530

    sample actions

--- src/containers/Root/App/Watch/actions.js
+++ src/containers/Root/App/Watch/actions.js
@@ -1,3 +1,24 @@
 /**
  * Created by jyothi on 16/2/17.
  */
+import { getVideoInfoAPI } from './api';
+
+export const getVideoInfo = (videoId) => {
+    return (dispatch, getState) => {
+
+        return dispatch({
+            types: [
+
+            ],
+            payload: {
+                promise: getVideoInfoAPI(user, appId, uspType, groupId, filters)
+                    .then((res) => {
+                        return res;
+                    }),
+            },
+            meta: {
+                //If Any
+            }
+        });
+    };
+};

10f6c87d22ca8a7cecc792d0fae4beb8aa8b9615
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-02-16 23:43:51 +0530

    sample api

--- src/containers/Root/App/Watch/api.js
+++ src/containers/Root/App/Watch/api.js
@@ -1,3 +1,23 @@
 /**
  * Created by jyothi on 16/2/17.
  */
+import { callApi } from '../../../../api';
+import { makeDefaultQueryString} from '../../../../utils';
+
+const VIDEO_INFO_API = "";
+
+export function getVideoInfoAPI(videoId, user) {
+    const url = makeDefaultQueryString(VIDEO_INFO_API, user, appId);
+    const config = {
+        method: "GET",
+        //body: JSON.stringify(filters)
+    };
+    const onSuccessCallback = json => {
+        return json;
+    };
+    const onFailureCallback = response => {
+        console.log("error", response.type, response.payload);
+        return [];
+    };
+    return callApi(url, config, onSuccessCallback, onFailureCallback);
+}

0b35d7f7074d21fe671593f690407a24c3c90485
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-03-19 19:16:29 +0530

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,49 +2,66 @@
  * Created by jyothi on 15/2/17.
  */
 import React, {Component} from 'react';
-import FontIcon from 'material-ui/FontIcon';
+import {END_POINTS} from '../constants/EndPoints';
 import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
 import Paper from 'material-ui/Paper';
 import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
+import FaceIcon from 'material-ui/svg-icons/action/face';
+import HelpIcon from 'material-ui/svg-icons/action/help';
+import LockIcon from 'material-ui/svg-icons/action/lock';
+import ContactMailIcon from 'material-ui/svg-icons/communication/contact-mail';
+import AssignmentIcon from 'material-ui/svg-icons/action/assignment';
+
+const footerStyles = {
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
+    left: 0,
+    //padding: '1rem',
+    width: '100%'
+};
 
 export default class Footer extends Component {
     state = {
-        selectedIndex: 0,
+        selectedIndex: 10,
     };
 
-    select = (index) => this.setState({selectedIndex: index});
+    //select = (index) => this.setState({selectedIndex: index});
+    handleClick = (endPoint) => this.props.router.push(`/${endPoint}`);
 
     render() {
         return (
-            <Paper zDepth={1}>
-                <BottomNavigation selectedIndex={this.state.selectedIndex}>
-                    <BottomNavigationItem
-                        label="About"
-                        icon={<IconLocationOn/>}
-                        onTouchTap={() => this.select(0)}
-                    />
-                    <BottomNavigationItem
-                        label="Help"
-                        icon={<IconLocationOn/>}
-                        onTouchTap={() => this.select(1)}
-                    />
-                    <BottomNavigationItem
-                        label="Contact"
-                        icon={<IconLocationOn/>}
-                        onTouchTap={() => this.select(2)}
-                    />
-                    <BottomNavigationItem
-                        label="Terms"
-                        icon={<IconLocationOn/>}
-                        onTouchTap={() => this.select(3)}
-                    />
-                    <BottomNavigationItem
-                        label="Privacy"
-                        icon={<IconLocationOn/>}
-                        onTouchTap={() => this.select(4)}
-                    />
-                </BottomNavigation>
-            </Paper>
+            <div style={footerStyles}>
+                <Paper zDepth={1}>
+                    <BottomNavigation selectedIndex={this.state.selectedIndex}>
+                        <BottomNavigationItem
+                            label="About"
+                            icon={<FaceIcon />}
+                            onClick={() => this.handleClick(END_POINTS.ABOUT)}
+                        />
+                        <BottomNavigationItem
+                            label="Help"
+                            icon={<HelpIcon />}
+                            onClick={() => this.handleClick(END_POINTS.HELP)}
+                        />
+                        <BottomNavigationItem
+                            label="Contact"
+                            icon={<ContactMailIcon />}
+                            onClick={() => this.handleClick(END_POINTS.CONTACT)}
+                        />
+                        <BottomNavigationItem
+                            label="Terms"
+                            icon={<AssignmentIcon />}
+                            onClick={() => this.handleClick(END_POINTS.TERMS)}
+                        />
+                        <BottomNavigationItem
+                            label="Privacy"
+                            icon={<LockIcon />}
+                            onClick={() => this.handleClick(END_POINTS.PRIVACY)}
+                        />
+                    </BottomNavigation>
+                </Paper>
+            </div>
         );
     }
 }

daf07330593c025d7eb7a7b7eab0b85b24c57619
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-03-19 17:42:02 +0530

    static content containers

--- src/containers/Root/App/StaticContent/SEO/Repeat.js
+++ src/containers/Root/App/StaticContent/SEO/Repeat.js
@@ -1,3 +1,57 @@
 /**
  * Created by jyothi on 19/3/17.
  */
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+function mapStateToProps(state) {
+    return {
+        session: state.session,
+        router: state.routing,
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+
+    };
+}
+
+class Repeat extends Component {
+
+    componentWillMount(){
+
+    }
+
+    componentWillUnmount(){
+
+    }
+
+    render() {
+        const { appState, params, location } = this.props;
+        return (
+            <section className="content row">
+                <div className="col-lg-2 col-md-2 col-sm-1 col-xs-1"></div>
+                <div className="col-lg-8 col-md-8 col-sm-10 col-xs-10">
+                    {/*TODO:*/}
+                </div>
+                <div className="col-lg-2 col-md-2">
+
+                </div>
+            </section>
+        );
+    }
+}
+
+Repeat.propTypes = {
+
+};
+
+Repeat.contextTypes = {
+    router: PropTypes.object.isRequired,
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Repeat);
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import {Route, IndexRedirect} from 'react-router';
-import Application from './containers/Root/App'
-import Watch from './containers/Root/App/Watch'
+import {END_POINTS} from './constants/EndPoints';
+import Application from './containers/Root/App';
+import Watch from './containers/Root/App/Watch';
+import About from './containers/Root/App/StaticContent/About';
+import Help from './containers/Root/App/StaticContent/Help';
+import Terms from './containers/Root/App/StaticContent/Terms';
+import Privacy from './containers/Root/App/StaticContent/Privacy';
+import Download from './containers/Root/App/StaticContent/SEO/Download';
+import Repeat from './containers/Root/App/StaticContent/SEO/Repeat';
+
 
 const router =
     <Route path="/" component={Application}>
-        <IndexRedirect to="watch"/>
-        <Route path="watch" component={Watch} />
-    </Route>
+        <IndexRedirect to={END_POINTS.WATCH}/>
+        <Route path={END_POINTS.WATCH} component={Watch} />
+        <Route path={END_POINTS.ABOUT} component={About} />
+        <Route path={END_POINTS.CONTACT} component={Watch} />
+        <Route path={END_POINTS.HELP} component={Help} />
+        <Route path={END_POINTS.TERMS} component={Terms} />
+        <Route path={END_POINTS.PRIVACY} component={Privacy} />
+        <Route path={END_POINTS.SEO_DOWNLOAD} component={Download} />
+    </Route>;
 
 export default router;

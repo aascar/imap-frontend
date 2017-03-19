451810fcf6c5500edb71c4cdbd93a3592382aae6
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-03-19 17:41:24 +0530

    site routes

--- src/constants/EndPoints.js
+++ src/constants/EndPoints.js
@@ -1,3 +1,15 @@
 /**
  * Created by jyothi on 19/3/17.
  */
+
+export const END_POINTS = {
+    HOME: "/",
+    WATCH: "watch",
+    ABOUT: "about",
+    CONTACT: "contact",
+    TERMS: "terms",
+    PRIVACY: "privacy",
+    HELP: "help",
+    SEO_DOWNLOAD: "how_to_download_youtube_video",
+    SEO_REPEAT: "how_to_repeat_youtube_video"
+};
